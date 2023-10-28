939c2a023af22e3f5f73c869b2d7cf4753562904
Milos Vasic <i@mvasic.ru>
2023-10-28 15:43:07 +0300

    Installation.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+HERE=$(pwd)
+
 echo "Installing the HelixTrack Screensaver"
 
-INSTALL_INTO=""
+SVG="helixtrack.svg"
+DIR_PIXMAPS="/usr/share/pixmaps"
+SCREENSAVER="helixtracklogo-floaters.desktop"
+INSTALL_INTO="/usr/share/applications/screensavers"
 
 if [ -n "$HELIX_TRACK_SCREENSAVER_DIR" ]; then
 
@@ -10,6 +15,36 @@ if [ -n "$HELIX_TRACK_SCREENSAVER_DIR" ]; then
 fi
 
 echo "Installing into: $INSTALL_INTO"
+
+if ! test -e "$INSTALL_INTO"; then
+
+    echo "ERROR: Directory does not exist '$INSTALL_INTO'"
+    exit 1
+fi
+
+if test -e "$INSTALL_INTO/$SCREENSAVER"; then
+
+    echo "HelixTrack Screensaver is already installed"
+    exit 0
+fi
+
+if ! test -e "$DIR_PIXMAPS"; then
+
+    if ! mkdir -p "$DIR_PIXMAPS"; then
+
+        echo "ERROR: Could not create directory '$DIR_PIXMAPS'"
+        exit 1
+    fi
+fi
+
+if ! test -e "$DIR_PIXMAPS/$SVG"; then
+
+    if ! cp "$HERE/$SVG" "$DIR_PIXMAPS"; then
+
+        echo "ERROR: Could not copy '$HERE/$SVG' into '$DIR_PIXMAPS' directory"
+        exit 1
+    fi
+fi
 
 echo "ERROR: Not implemented"
 exit 1
--- Recipes/prepare.sh
+++ Recipes/prepare.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 echo "Preparing the HelixTrack Screensaver installation"
+echo "Nothing to prepare ..."
